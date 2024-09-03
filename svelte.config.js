e2ca4701db80c13d7070263d7127f6740aafefec
frknue <furkan.uelker@hotmail.com>
2024-09-03 17:01:49 +0200

    rollback

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 import { escapeSvelte, mdsvex } from "mdsvex";
 import shiki from "shiki";
@@ -80,11 +80,7 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(
-      {
-        runtime: "nodejs18.x",
-      }
-    )
+    adapter: adapter()
   }
 };
 
