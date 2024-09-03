675c5fc3049256dfde794c8a3d4ebdc48e5191ca
frknue <furkan.uelker@hotmail.com>
2024-09-03 16:59:50 +0200

    testing vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from "@sveltejs/kit/vite";
 import { escapeSvelte, mdsvex } from "mdsvex";
 import shiki from "shiki";
@@ -63,7 +63,6 @@ async function codeHighlighter(code, langStr) {
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  runtime: 'nodejs18.x',
   extensions: [".svelte", ".md"],
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
@@ -81,7 +80,11 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter()
+    adapter: adapter(
+      {
+        runtime: "nodejs18.x",
+      }
+    )
   }
 };
 
