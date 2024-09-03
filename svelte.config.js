7f8395f7b431db19a46143525f9c6f145a1d5ec8
frknue <furkan.uelker@hotmail.com>
2024-09-03 17:32:20 +0200

    migrated svelte kit

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from "@sveltejs/adapter-auto";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import adapter from "@sveltejs/adapter-vercel";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { escapeSvelte, mdsvex } from "mdsvex";
 import shiki from "shiki";
 import { loadTheme } from "shiki-themes";
@@ -80,9 +80,7 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter({
-      runtime: 'nodejs18.x',
-    }),
+    adapter: adapter()
   }
 };
 
